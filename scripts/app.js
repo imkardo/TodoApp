69054b315ee134fe2db9c15f982e2d9005c647b8
imkardo <kardoheydari.1387@gmail.com>
2025-12-29 18:52:03 +0330

    Filter Todos

--- scripts/app.js
+++ scripts/app.js
@@ -3,9 +3,11 @@ let todos = [];
 const todoForm = document.querySelector(".todo-form");
 const todoInput = document.querySelector(".todo-input");
 const todoList = document.querySelector(".todo-list");
+const selectFilter = document.querySelector(".filter-todos");
+
 // Events
 todoForm.addEventListener("submit", addNewTodo);
-
+selectFilter.addEventListener("change", filterTodos);
 // Functions
 function addNewTodo(e) {
   e.preventDefault();
@@ -20,23 +22,52 @@ function addNewTodo(e) {
   };
 
   todos.push(newTodo);
+  createTodos(todos);
+}
+
+function createTodos(todos) {
   let result = "";
-  todos.forEach((item) => {
+  todos.forEach((todo) => {
     result += `<li class="todo">
-        <span><p class="todo__title ">${item.title}</p>
+        <span><p class="todo__title ">${todo.title}</p>
           <span class="todo__createdAt">${new Date(
-            item.createdAt
+            todo.createdAt
           ).toLocaleDateString("fa-IR")}</span></span>
          <span>
           <button class="todo__check" data-todo-id=${
-            item.id
+            todo.id
           } ><i class="far fa-check-square"></i></button>
           <button class="todo__remove" data-todo-id=${
-            item.id
+            todo.id
           } ><i class="far fa-trash-alt"></i></button>
          </span>
         </li>`;
-    todoList.innerHTML = result;
-    todoInput.value = "";
   });
+  todoList.innerHTML = result;
+  todoInput.value = "";
+}
+
+function filterTodos(e) {
+  const filter = e.target.value;
+  switch (filter) {
+    case "all":
+      {
+        createTodos(todos);
+      }
+      break;
+    case "completed":
+      {
+        const filteredTodos = todos.filter((t) => t.isCompleted);
+        createTodos(filteredTodos);
+      }
+      break;
+    case "uncompleted":
+      {
+        const filteredTodos = todos.filter((t) => !t.isCompleted);
+        createTodos(filteredTodos);
+      }
+      break;
+    default:
+      createTodos(todos);
+  }
 }
